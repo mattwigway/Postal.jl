83555d5cf6dd286c732e81c7e5aaabbf0fa93dc0
Matt Bhagat-Conway <matt@indicatrix.org>
2024-03-25 23:25:50 -0400

    add teardown

--- src/Postal.jl
+++ src/Postal.jl
@@ -8,14 +8,6 @@ include("expand.jl")
 
 include("parse.jl")
 
-function __init__()
-    if Threads.nthreads() > 1
-        @warn "Libpostal is not thread safe but running with multiple threads; ensure you do not call libpostal from multiple threads"
-    end
-
-    @eval const _status = PostalStatus(false, false)
-end
-
-export setup_libpostal
+export setup_libpostal, expand_address, parse_address, teardown_libpostal
 
 end
--- src/setup.jl
+++ src/setup.jl
@@ -3,10 +3,22 @@ mutable struct PostalStatus
     torn_down::Bool
 end
 
-function setup_libpostal(data_path::String)
-    if _status.setup
-        error("Libpostal already set up")
+function __init__()
+    if Threads.nthreads() > 1
+        @warn "Libpostal is not thread safe but running with multiple threads; ensure you do not call libpostal from multiple threads"
     end
+
+    @eval const _status = PostalStatus(false, false)
+end
+
+function check_postal_setup()
+    Postal.status.setup || error("libpostal_setup not run")
+    Postal.status.torn_down && error("libpostal already torn down (restart julia)")
+end
+
+function setup_libpostal(data_path::String)
+    Postal._status.setup && error("Libpostal already set up")
+    Postal._status.torn_down && error("Libpostal already torn down (restart julia)")
 
     setup_stat = @ccall libpostal.libpostal_setup_datadir(data_path::Cstring)::Bool
 
@@ -26,5 +38,12 @@ function setup_libpostal(data_path::String)
         error("Libpostal language classifier setup failed")
     end
 
-    _status.setup = true
+    Postal._status.setup = true
+end
+
+function teardown_libpostal()
+    @ccall libpostal.libpostal_teardown()::Cvoid
+    @ccall libpostal.libpostal_teardown_parser()::Cvoid
+    @ccall libpostal.libpostal_teardown_classifier()::Cvoid
+    Postal._status.torn_down = true
 end
